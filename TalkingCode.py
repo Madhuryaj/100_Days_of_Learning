2467fe13acdd6f5bf1e6b0bc8e5c74359885bde0
Madhurya <33352186+Madhuryaj@users.noreply.github.com>
2020-10-12 21:04:35 +0530

    Create TalkingCode.py

---
+++ TalkingCode.py
@@ -0,0 +1,7 @@
+import pyttsx3
+
+# python code to make your code to talk i.e, Text to Speech
+talk = pyttsx3.init()
+talk.say("Hello! Thank You for checking out my Repository ,How are you doing today?All the best for your Programming "
+         "Journey")
+talk.runAndWait()
